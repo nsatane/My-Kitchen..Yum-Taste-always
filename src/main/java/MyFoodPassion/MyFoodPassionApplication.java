a11dbd8264d03836e51470e1708a65e04745ec80
Neha Bhalke <nsatane@gmail.com>
2019-01-24 15:53:21 -0600

    added some folders

---
+++ src/main/java/MyFoodPassion/MyFoodPassionApplication.java
@@ -0,0 +1,14 @@
+package MyFoodPassion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFoodPassionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFoodPassionApplication.class, args);
+	}
+
+}
+
